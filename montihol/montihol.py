eb54edfac8d74534285f70f55f9d302627855c99
nel951 <inwooha00@naver.com>
2021-12-16 21:16:55 +0900

    버그 수정, 기다리기 기능 추가

--- montihol/montihol.py
+++ montihol/montihol.py
@@ -6,7 +6,18 @@ print('''
 선택지를 바꾸는 경우 / 바꾸지 않는 경우의 정/오답 비율을 확인해 볼 수 있습니다
 ''')
 
-sleep(3)
+sle = str(input('매 결과마다 기다리기를 하시겠습니까? (y/n) >>>'))
+if sle == 'y': print('결과가 나올때마다 0.3초간 기다립니다')
+
+def pick(l,fans):
+        
+        l.pop(l.index(fans))
+            
+        return l[0]
+
+def sl(a:float):
+    if sle != 'y' : return
+    sleep(a)
 
 def main(a):
     cy = 1000
@@ -16,13 +27,7 @@ def main(a):
 
     sleep(1)
     
-    def pick(l):
-        ans = random.randint(1,3)
-        try:
-            l.pop(ans)
-        except:
-            pick(l)
-        return ans
+    
     
 
 
@@ -40,12 +45,13 @@ def main(a):
             answer = l[n]
             while True:
                 b = random.randint(0,2)
-                if l[b]!= answer:
+                if l[b]!= answer and l[b] != ans:
                     print(f'선텍지 "{l[b]}"을(를) 제외합니다')
                     l.pop(b)
                     break
-            print('선텍지를 바꿉니다')    
-            ans = pick(l)
+                
+            print('선텍지를 바꿉니다') 
+            ans = pick(l,ans)
             print(f'선텍지 : {ans}')
             if ans == answer :
                 print('정답')
@@ -53,6 +59,7 @@ def main(a):
             else:
                 print('오답')
                 fai += 1
+            sl(0.7)
 
     elif a == 'n':
             print('선텍지를 바꾸지 않으며 시뮬레이션을 진행합니다')
@@ -73,6 +80,8 @@ def main(a):
                 else:
                     print('오답')
                     fai += 1
+                sl(0.7)
+
     perS = sus/(sus+fai)*100
     perF = fai/(sus+fai)*100
     print(f'''
@@ -84,7 +93,7 @@ def main(a):
     오답 비율 : {perF}%
 
     ''')
-    sleep(5)
+    sl(5)
     return perS
 
 print(f'''
